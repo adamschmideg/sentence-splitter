d7cf95449884bfa7cd69f6b3efe8949d6df4d98d
Adam Schmideg <adam@schmideg.net>
2018-07-14 14:02:45 +0200

    almost empty demo webpage

---
+++ examples/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import SentenceSplitterComponent from "../../src";
+
+const App = () => <SentenceSplitterComponent />;
+
+render(<App />, document.getElementById("root"));
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./styles.css";
+
+const SentenceSplitterComponent = () => <h1>The sentence splitter</h1>;
+export default SentenceSplitterComponent;
